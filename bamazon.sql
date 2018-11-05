5a77bd86468ee8347b29c51b97cbcb2c2d3d4db4
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-11-05 10:06:46 -0600

    adding packages

--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE bamazon
+
+USE bamazon
+
+CREATE TABLE products
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(255) NOT NULL,
+department_name VARCHAR(255) NOT NULL,
+price INT
+stock_quantity INT
